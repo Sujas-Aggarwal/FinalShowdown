c5c90a71fe523ad861f8a666de9dac9128a6c9a4
Sujas <ce1221047@iitd.ac.in>
2024-12-18 17:57:47 +0530

    basic done
    - Collision detection and conversion left

--- main.cpp
+++ main.cpp
@@ -3,8 +3,22 @@
 #include <random>
 #include <math.h>
 using namespace std;
+const int SCREEN_WIDTH = 960;
+const int SCREEN_HEIGHT = 960;
+const int PLAYER_COUNT = 10; // of every kind
+const int MAX_SPEED = 3;     // this is for 60fps, i don't think there is any need nor time to add delta time in such a small project
+Color BACKGROUND_COLOR = Color{242, 238, 203, 255};
+Image paper = LoadImage("assets/paper.png");
+Image scissor = LoadImage("assets/scissor.png");
+Image stone = LoadImage("assets/stone.png");
 
-pair<int, int> createRandomPoint(int minX, int maxX,int minY,int maxY)
+enum class GameObjectType
+{
+    PAPER,
+    SCISSOR,
+    STONE
+};
+pair<int, int> genRandVal(int minX, int maxX, int minY, int maxY)
 {
     // Generating a random coordinate for the point to display
     random_device rd;
@@ -12,60 +26,118 @@ pair<int, int> createRandomPoint(int minX, int maxX,int minY,int maxY)
     uniform_int_distribution<> disX(minX, maxX);
     uniform_int_distribution<> disY(minY, maxY);
     int x_cordi = disX(gen);
+    if (x_cordi == 0)
+        x_cordi = disX(gen);
     int y_cordi = disY(gen);
+    if (y_cordi == 0)
+        y_cordi = disY(gen);
     return {x_cordi, y_cordi};
 }
-bool isInsideCircle(pair<int, int> &coordi, pair<int, int> origin, int radius)
+class GameObjects
 {
-    pair<int, int> shiftedOrigin = {origin.first - coordi.first, origin.second - coordi.second};
-    // checking the basic condition of x^2 + y^2 < r^2 for a circle (i am here taking on the circle as outside circle too)
-    if (radius >= sqrt(shiftedOrigin.first*shiftedOrigin.first + shiftedOrigin.second*shiftedOrigin.second)){
-        return true;
+private:
+    pair<int, int> velocity; // vector
+    pair<int, int> position; // point
+    Texture2D texture;
+    GameObjectType type;
+    const int radius = 25;
+
+public:
+    GameObjects(GameObjectType type)
+    {
+        this->velocity = genRandVal(-MAX_SPEED, MAX_SPEED, -MAX_SPEED, MAX_SPEED);
+        this->position = genRandVal(this->radius, SCREEN_WIDTH - this->radius, this->radius, SCREEN_HEIGHT - this->radius);
+        this->type = type;
+        switch (type)
+        {
+        case GameObjectType::PAPER:
+            if (paper.height != this->radius * 2 || paper.width != this->radius * 2)
+            {
+                ImageResize(&paper, this->radius * 2, this->radius * 2);
+                ImageRotateCCW(&paper);
+            }
+            this->texture = LoadTextureFromImage(paper);
+            break;
+        case GameObjectType::SCISSOR:
+            if (scissor.height != this->radius * 2 || scissor.width != this->radius * 2)
+            {
+                ImageResize(&scissor, this->radius * 2, this->radius * 2);
+                ImageRotateCCW(&scissor);
+            }
+            this->texture = LoadTextureFromImage(scissor);
+            break;
+        case GameObjectType::STONE:
+            if (stone.height != this->radius * 2 || stone.width != this->radius * 2)
+            {
+                ImageResize(&stone, this->radius * 2, this->radius * 2);
+                ImageRotateCCW(&stone);
+            }
+            this->texture = LoadTextureFromImage(stone);
+            break;
+        }
     }
-    return false;
-}
+    ~GameObjects()
+    {
+        UnloadTexture(texture);
+    }
+    void update()
+    {
+        position.first += velocity.first;
+        position.second += velocity.second;
+        if (position.first <= this->radius || position.first >= SCREEN_WIDTH - this->radius)
+        {
+            velocity.first *= -1;
+        }
+        if (position.second <= this->radius || position.second >= SCREEN_HEIGHT - this->radius)
+        {
+            velocity.second *= -1;
+        }
+    }
+    void draw()
+    {
+        DrawTexture(texture, position.first - this->radius, position.second - this->radius, WHITE);
+    }
+};
 int main()
 {
-    const int SCREEN_WIDTH = 650;
-    const int SCREEN_HEIGHT = 650;
-    const int SQUARE_SIZE = 600;
-    int insideDots = 0;
-    int outsideDots = 0;
-    int totalDots = 0;
-    float mostAccuratePi = 0;
-    float leastError = 100;
-    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Monte Carlo Simulation");
-    SetTargetFPS(120);
+
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Final Showdown");
+    SetTargetFPS(60);
+
+    vector<GameObjects *> gameObjects(3 * PLAYER_COUNT);
+    for (int i = 0; i < PLAYER_COUNT; i++)
+    {
+        gameObjects[i] = new GameObjects(GameObjectType::PAPER);
+    }
+    for (int i = PLAYER_COUNT; i < 2 * PLAYER_COUNT; i++)
+    {
+        gameObjects[i] = new GameObjects(GameObjectType::STONE);
+    }
+    for (int i = 2 * PLAYER_COUNT; i < 3 * PLAYER_COUNT; i++)
+    {
+        gameObjects[i] = new GameObjects(GameObjectType::SCISSOR);
+    }
     while (!WindowShouldClose())
     {
+        // input toh kuch h nahi since simulation h more or less
+        // update
+        for (int i = 0; i < 3 * PLAYER_COUNT; i++)
+        {
+            gameObjects[i]->update();
+        }
         BeginDrawing();
-        DrawRectangleLines((SCREEN_WIDTH - SQUARE_SIZE) / 2, (SCREEN_HEIGHT - SQUARE_SIZE) / 2, SQUARE_SIZE, SQUARE_SIZE, WHITE);
-        pair<int, int> newPoint = createRandomPoint((SCREEN_WIDTH - SQUARE_SIZE) / 2, (SCREEN_WIDTH + SQUARE_SIZE) / 2,(SCREEN_HEIGHT - SQUARE_SIZE) / 2, (SCREEN_HEIGHT + SQUARE_SIZE) / 2);
-        totalDots++;
-        bool isInside = isInsideCircle(newPoint,{SCREEN_WIDTH/2, SCREEN_HEIGHT/2},SQUARE_SIZE/2);
-        if (isInside){
-            insideDots++;
+        gameObjects[0]->update();
+        ClearBackground(BACKGROUND_COLOR);
+        for (int i = 0; i < 3 * PLAYER_COUNT; i++)
+        {
+            gameObjects[i]->draw();
         }
-        else{
-            outsideDots++;
-        }
-        DrawCircle(newPoint.first, newPoint.second, 1, isInside? RED : BLUE);
-        cout<<"--------------------------------------------"<<endl;
-        cout<<"Total Dots: "<<totalDots<<endl;
-        cout<<"Dots Inside Circle: "<<insideDots<<endl;
-        cout<<"Dots Outside Circle: "<<outsideDots<<endl;
-        float calcPi = 4*(float)insideDots/(float)totalDots;
-        float error = abs(PI-calcPi)*100/PI;
-        cout<<"Estimated Pi: "<<calcPi<<endl;
-        cout<<"Error:" <<error<<"%"<<endl;
-        if (error<leastError){
-            leastError = error;
-            mostAccuratePi = calcPi;
-        }
-        cout<<"--------------------------------------------"<<endl;
+        gameObjects[0]->draw();
+        // draw
         EndDrawing();
     }
-    cout<<"Most Accurate PI during Simulation was "<<mostAccuratePi<<endl;
-    cout<<"With Error Percentage of "<<leastError<<"%"<<endl;
+    UnloadImage(paper);
+    UnloadImage(scissor);
+    UnloadImage(stone);
     CloseWindow();
 }
